49796bb586d53b416ca7258323c6888a36e5f260
Vaibhav Singh <vaibhav05dec@gmail.com>
2026-05-15 13:50:17 +0530

    feat: enhance tokenization script with context sampling and output formatting

--- Lec-09/9.py
+++ Lec-09/9.py
@@ -3,4 +3,21 @@ encoding = tiktoken.get_encoding("gpt2")
 with open("./Lec-07/the-verdict.txt", "r") as f:
     rawtext = f.read()
     tokens = encoding.encode(rawtext)
-    print(len(tokens))
+    # print(len(tokens))
+
+    context_size = 4
+    enc_sample = tokens[50:] # for tokens
+    enc_sameple1 = rawtext[50:] # for raw text
+    print("text:", enc_sameple1[:context_size])
+    print("text:", enc_sameple1[1:context_size+1])
+    x=enc_sample[:context_size]
+    y=enc_sample[1:context_size+1]
+    print("x:", x)
+    print("y:", y)
+
+
+    # context and desired using for loop
+    for i in range(1,context_size+1):
+        x=enc_sample[:i] # context
+        y=enc_sample[i] # desired
+        print(x,"----->",y)
